931f6d2953261567bac4acc0df48459508e197be
SarcasticFrog <s4rc4st1c.fr0g@gmail.com>
2026-05-29 09:32:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
 
+self.addEventListener('fetch', (event) => {
+    // Erlaubt es der App, Daten zu laden
+    event.respondWith(fetch(event.request));
+});
